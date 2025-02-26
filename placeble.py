8864886380b0d23fd2c8748145624ae2ac0582f5
Flapjack2000 <148223049+Flapjack2000@users.noreply.github.com>
2025-02-26 14:08:32 -0500

    Update placeble.py
    File name misspelling warning.

--- placeble.py
+++ placeble.py
@@ -1,1 +1,3 @@
-
+# Yes, this file is spelled incorrectly. It should be spelled "placeable" with an "a".
+# That's how it appears in the submission field on PrarieLearn.
+# It should be fine, since we'll only need to access the classes within the files.
