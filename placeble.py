850df8abb35e9d099bcbb60375ea1a47d9781b63
Flapjack2000 <148223049+Flapjack2000@users.noreply.github.com>
2025-03-03 09:20:11 -0500

    started Placeble class
    Need to fix:
    1) Circular import between placeble and game_piece due to Placeble's type hint containing GamePiece and GamePiece's inheritance from Placeble.
    
    2) Finish Placeble.is_valid_placement(). Need to figure out how to dynamically get board dimensions and check for ok placements.

--- placeble.py
+++ placeble.py
@@ -1,3 +1,35 @@
-# Yes, this file is spelled incorrectly. It should be spelled "placeable" with an "a".
-# That's how it appears in the submission field on PrarieLearn.
-# It should be fine, since we'll only need to access the classes within the files.
+from abc import ABC, abstractmethod
+from player_colors import PlayerColors
+from position import Position
+from typing import List, Optional
+
+# FIXME: Causes circular import since GamePiece relies on Placeble
+from game_piece import GamePiece
+
+class Placeble(ABC):
+    def __init__(self, color: PlayerColors):
+        # uses setter to check errors
+        self.color: PlayerColors = color
+
+    @property
+    def color(self) -> PlayerColors:
+        return self.__color
+
+    @color.setter
+    def color(self, color: PlayerColors):
+        # TODO: raise ValueError for bad color???
+        if not isinstance(color, PlayerColors):
+            raise TypeError('Color must be of type PlayerColors.')
+        self.__color: PlayerColors = color
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+    @abstractmethod
+    def is_valid_placement(self, pos: Position, board: List[List[Optional[GamePiece]]]) -> bool:
+        if not isinstance(pos, Position):
+            return False
+        # TODO: validate selected pos is on the board
+        # TODO: validate selected pos is currently empty
+        return True
