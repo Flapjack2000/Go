3d7746e2c824349f897f6834f4827c9a492337d4
Flapjack2000 <148223049+Flapjack2000@users.noreply.github.com>
2025-03-06 16:01:59 -0500

    Finish basic placement validation in game_piece.py

--- game_piece.py
+++ game_piece.py
@@ -8,31 +8,28 @@ class GamePiece(Placeble):
         self.color = color
 
     def is_valid_placement(self, pos: Position, board: list[[]]):
-        super().is_valid_placement(pos, board)
-        self.pos = pos
-        self.board = board
+        if super().is_valid_placement(pos, board):
+            neighbor_positions = [Position(pos.row - 1, pos.col), Position(pos.row, pos.col - 1), Position(pos.row + 1, pos.col), Position(pos.row, pos.col + 1)]
+            for nb_pos in neighbor_positions:
+                # The piece or None at the neighboring position
+                nb_piece = board[nb_pos.row][nb_pos.col]
 
-        around_piece = [[(self.pos[0]) - 1, self.pos[1]], [(self.pos[0]) - 1, (self.pos[1]) - 1], [self.pos[0], (self.pos[1]) - 1], [(self.pos[0]) + 1, (self.pos[1]) - 1], [(self.pos[0]) + 1, self.pos[1]], [(self.pos[0]) + 1, (self.pos[1]) + 1], [self.pos[0], (self.pos[1]) + 1], [(self.pos[0]) - 1, (self.pos[1]) + 1]]
-        for piece in around_piece:
-            if board[piece[0]][piece[1]] == None:
-                return True
-            if board[piece[0]][piece[1]] != None:
-                pass
-                #check if is player color or enemy color, if enemy enemy_counter += 1
-                #if to run is_valid_placement again if the "piece" being checked is a player piece, maybe add counter for how many player pieces to multiply by
-                #number of possible surrounding pieces would be num player pieces times 8, minus 1 for each call of is_valid_placement
-                #return false if enemy counter = possible surrounding pieces
+                # check if the neighboring position is empty
+                if super().is_valid_placement(nb_pos, board):
+                    return True
+
+                # check if the neighboring position is a wall
+                elif not super().is_valid_placement(nb_pos, board) and not isinstance(nb_piece, PlayerColors):
+                    pass
+
+                # check if the stone at the neighboring position is the current player's color
+                elif nb_piece.color == self.color:
+                    return True
+            return False
+        return False
 
     def __eq__(self, other):
-        #not sure what to put after self and other
-        gamepiece1 = self.color
-        gamepiece2 = other.color
-
-        if gamepiece1 == gamepiece2:
-            return True
-        else:
-            return False
-
+        return self.color == other.color
 
     def __str__(self):
-        return f"Player {self.color} at {self.pos}"
+        return f"{self.color} Piece"
