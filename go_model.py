9875e2ec576ae1f0856af958d86b6c326d60cf19
flapjackzach <148223049+Flapjack2000@users.noreply.github.com>
2025-03-08 12:36:10 -0500

    put board size validation in appropriate setters.

--- go_model.py
+++ go_model.py
@@ -1,7 +1,7 @@
-
 from game_piece import GamePiece
 from game_player import GamePlayer
 from player_colors import PlayerColors
+
 
 class GoModel:
     def __init__(self, nrows: int = 6, ncols: int = 6):
@@ -9,22 +9,12 @@ class GoModel:
         self.current_player = GamePlayer(PlayerColors.BLACK)
 
         # Board dimensions
+        self.valid_board_lengths = (6, 9, 11, 13, 19)
         self.nrows = nrows
         self.ncols = ncols
 
         # Generate an empty board based on dimensions
         self.board = [[None] * self.ncols] * self.nrows
-
-        # Board dimension validation
-        if not isinstance(nrows, int):
-            raise TypeError('Number of rows must be a positive integer.')
-        if not isinstance(ncols, int):
-            raise TypeError('Number of columns must be a positive integer.')
-
-        if nrows not in self.valid_board_lengths:
-            raise ValueError(f"Number of rows must be one of {self.valid_board_lengths}.")
-        if ncols not in self.valid_board_lengths:
-            raise ValueError(f"Number of columns must be one of {self.valid_board_lengths}.")
 
         # TODO: Write initial message
         self.message = "First message"
@@ -35,7 +25,9 @@ class GoModel:
 
     @current_player.setter
     def current_player(self, player: GamePlayer):
-        # REVIEW: validation
+        # REVIEW: validation.
+        #   Is anything else needed?
+        #   Wrong colors will be checked for in GamePlayer's instantiation.
         if not isinstance(player, GamePlayer):
             raise TypeError('Players must be of type GamePlayer.')
         self.__current_player = player
@@ -46,7 +38,10 @@ class GoModel:
 
     @nrows.setter
     def nrows(self, nrows: int):
-        # TODO: validation
+        if not isinstance(nrows, int):
+            raise TypeError('Number of rows must be a positive integer.')
+        if nrows not in self.valid_board_lengths:
+            raise ValueError(f"Number of rows must be one of {self.valid_board_lengths}.")
         self.__nrows = nrows
 
     @property
@@ -55,7 +50,10 @@ class GoModel:
 
     @ncols.setter
     def ncols(self, ncols: int):
-        # TODO: validation
+        if not isinstance(ncols, int):
+            raise TypeError('Number of columns must be a positive integer.')
+        if ncols not in self.valid_board_lengths:
+            raise ValueError(f"Number of columns must be one of {self.valid_board_lengths}.")
         self.__ncols = ncols
 
     @property
@@ -80,22 +78,22 @@ class GoModel:
     # 1) Place piece
     # 2) Find neighboring opponent pieces 
     # 3) For each opponent piece found, find their ally neighbors. 
-        # a) Put each neighboring opponent piece in a set (of their own). 
-        # b) For each opponent piece in each set, add its neighboring allies to the set.
-        # c) Because it's a set and won't increase its cardinality forever 
-            # into infinity when it's added to, it should stop iterating when all members are found. 
-        # d) Alternatively, this might be a cool use case for recursion.
-        # Now we have our neighboring clusters (sets) of enemies 
-    
+    # a) Put each neighboring opponent piece in a set (of their own).
+    # b) For each opponent piece in each set, add its neighboring allies to the set.
+    # c) Because it's a set and won't increase its cardinality forever
+    # into infinity when it's added to, it should stop iterating when all members are found.
+    # d) Alternatively, this might be a cool use case for recursion.
+    # Now we have our neighboring clusters (sets) of enemies
+
     # 4) Check if the clusters are surrounded 
-        # a) Clusters are surrounded if all of their members neighbor only:
-            # i) Their cluster-mates
-            # ii) Enemy pieces 
-            # iii) Board edges
-        # b) Perhaps that can be restated:
-            # If none of the pieces in the cluster touch an empty place, 
-            # then the cluster is surrounded.
-    
+    # a) Clusters are surrounded if all of their members neighbor only:
+    # i) Their cluster-mates
+    # ii) Enemy pieces
+    # iii) Board edges
+    # b) Perhaps that can be restated:
+    # If none of the pieces in the cluster touch an empty place,
+    # then the cluster is surrounded.
+
     # SAVE BOARD
     # Keep list of board copies (not deep copies)
     # Might need to track other data like scores
@@ -106,5 +104,3 @@ class GoModel:
     # Undo: 
     #     - board = past_boards[index - 1]
     #     - index--
-
-
