745656b1a41869cb89b436bb44c0984d61ca5a9b
flapjackzach <148223049+Flapjack2000@users.noreply.github.com>
2025-03-16 18:55:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,34 @@
+import player_colors
+from game_piece import GamePiece
+from go_model import GoModel
+from player_colors import PlayerColors
+from position import Position
 
+b = PlayerColors.BLACK
+w = PlayerColors.WHITE
+
+board = [[None,         None,       None,           None,           None,           None],
+         [None,         None,       None,           GamePiece(b),   None,           None],
+         [None,         None,       GamePiece(w),   None,           GamePiece(w),   None],
+         [None,         None,       None,           GamePiece(w),   None,           None],
+         [None,         None,       None,           None,           None,           None],
+         [None,         None,       None,           None,           None,           None]]
+
+board2= [[None,         None,       None,           None,           None,           None],
+         [None,         None,       None,           GamePiece(b),   None,           None],
+         [None,         None,       GamePiece(w),   None,           GamePiece(w),   None],
+         [None,         None,       None,           GamePiece(w),   None,           None],
+         [None,         None,       None,           None,           None,           None],
+         [None,         None,       None,           None,           None,           None]]
+
+
+x = GoModel()
+x.set_piece(Position(1, 3), GamePiece(b))
+x.set_next_player()
+x.set_piece(Position(3, 3), GamePiece(w))
+x.set_next_player()
+x.set_piece(Position(2, 2), GamePiece(w))
+x.set_next_player()
+x.set_piece(Position(2, 4), GamePiece(w))
+
+print(x.is_same_board(board, board2))
