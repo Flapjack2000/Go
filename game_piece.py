4634ee4933f828dd14212fcef35e0f0805f02732
flapjackzach <148223049+Flapjack2000@users.noreply.github.com>
2025-03-18 09:36:53 -0400

    add flag methods

--- game_piece.py
+++ game_piece.py
@@ -3,40 +3,51 @@ from player_colors import PlayerColors
 from position import Position
 
 class GamePiece(Placeble):
-    def __init__(self, color: PlayerColors):
-        super().__init__(color)
+	def __init__(self, color: PlayerColors):
+		super().__init__(color)
 
-    def is_valid_placement(self, pos: Position, board):
-        # Basic validation
-        if not super().is_valid_placement(pos, board):
-            return False
+		self.flag = False
 
-        is_possible = False
+	def toggle_flag(self):
+		self.flag = not self.flag
 
-        neighbor_positions = [Position(pos.row - 1, pos.col), Position(pos.row, pos.col - 1), Position(pos.row + 1, pos.col), Position(pos.row, pos.col + 1)]
-        for nb_pos in neighbor_positions:
+	def raise_flag(self):
+		self.flag = True
 
-            # Check if the neighboring position is off the board, which means the piece is being placed on the edge
-            if not 0 <= nb_pos.row < len(board):
-                continue
-            if not 0 <= nb_pos.col < len(board[0]):
-                continue
+	def lower_flag(self):
+		self.flag = False
 
-            # Check if there is an empty space nearby
-            if board[nb_pos.row][nb_pos.col] is None:
-                is_possible = True
+	def is_valid_placement(self, pos: Position, board):
+		# Basic validation
+		if not super().is_valid_placement(pos, board):
+			return False
 
-            # Check if there is a piece of the same color nearby
-            elif board[nb_pos.row][nb_pos.col].color == self.color:
-                is_possible = True
+		is_possible = False
 
-        return is_possible
+		neighbor_positions = [Position(pos.row - 1, pos.col), Position(pos.row, pos.col - 1), Position(pos.row + 1, pos.col), Position(pos.row, pos.col + 1)]
+		for nb_pos in neighbor_positions:
 
-    def __eq__(self, other):
-        return self.color == other.color
+			# Check if the neighboring position is off the board, which means the piece is being placed on the edge
+			if not 0 <= nb_pos.row < len(board):
+				continue
+			if not 0 <= nb_pos.col < len(board[0]):
+				continue
 
-    def __str__(self):
-        return f"{self.color} Piece"
+			# Check if there is an empty space nearby
+			if board[nb_pos.row][nb_pos.col] is None:
+				is_possible = True
 
-    def __repr__(self):
-        return f'{self.color}'
+			# Check if there is a piece of the same color nearby
+			elif board[nb_pos.row][nb_pos.col].color == self.color:
+				is_possible = True
+
+		return is_possible
+
+	def __eq__(self, other):
+		return self.color == other.color
+
+	def __str__(self):
+		return f"{self.color} Piece"
+
+	def __repr__(self):
+		return f'{self.color}'
