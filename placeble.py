7a08516d81d4b5e3c75a25a4e0e7b4325b315517
flapjackzach <148223049+Flapjack2000@users.noreply.github.com>
2025-03-22 07:29:40 -0400

    ''' -> """

--- placeble.py
+++ placeble.py
@@ -1,15 +1,15 @@
-'''
+"""
 Author: Zach Williams and Evan Dahl
 date: 3/23/2025
 Title: Placeable Abstract Class
 Purpose: this file assigns a player color and starts checking for valid placement
-'''
+"""
 from abc import ABC, abstractmethod
 from player_colors import PlayerColors
 from position import Position
 
 class Placeble(ABC):
-    '''
+    """
     To be inherited by GamePiece and represents a placeable piece with its color
 
     Attributes:
@@ -17,9 +17,9 @@ class Placeble(ABC):
     Methods:
         color(): return the private variable color
         is_valid_placement(): returns Ture or False for basic placement conditions
-    '''
+    """
     def __init__(self, color: PlayerColors):
-        '''
+        """
         Initialize the placeble class as an instance of PlayerColors
 
         Args:
@@ -28,7 +28,7 @@ class Placeble(ABC):
             TypeError: if color is not an instance of PlayerColors
             ValueError: if color is not Back or White PlayerColors
 
-        '''
+        """
         # uses setter to check errors
         if not isinstance(color, PlayerColors):
             raise TypeError('Color must be of type PlayerColors.')
@@ -40,34 +40,34 @@ class Placeble(ABC):
 
     @property
     def color(self) -> PlayerColors:
-        '''
+        """
         Returns the private variable color
 
         Returns:
             PlayerColors: instance of player color that represents the players color either black or white
-        '''
+        """
         return self.__color
 
     @abstractmethod
     def __str__(self) -> str:
-        '''
+        """
         Starts the string method to be inherited by GamePiece
 
         Returns:
              str: The name of the GamePiece
-        '''
+        """
         pass
 
     @abstractmethod
     def is_valid_placement(self, pos: Position, board) -> bool:
-        '''
+        """
         Returns Ture or False for basic placement conditions
 
         Args:
             pos(Position): the coordinates of the desired placement
             board(list[list]): The current board state
 
-        '''
+        """
         # Check pos is a Position instance
         if not isinstance(pos, Position):
             return False
