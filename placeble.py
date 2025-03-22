be7f66acc0b0166eff110e0c670cdc50dab1a581
EDogTheDog <dahle@mail.gvsu.edu>
2025-03-22 00:00:37 -0400

    Docstrings placeble.py

--- placeble.py
+++ placeble.py
@@ -1,8 +1,23 @@
+'''
+Author: Zach Williams and Evan Dahl
+date: 3/23/2025
+Title: Placeable Abstract Class
+Purpose: this file assigns a player color and starts checking for valid placement
+'''
 from abc import ABC, abstractmethod
 from player_colors import PlayerColors
 from position import Position
 
 class Placeble(ABC):
+    '''
+    To be inherited by GamePiece and represents a placeable piece with its color
+
+    Attributes:
+        color (PlayerColors): an instance of player color that represents the players color either black or white
+    Methods:
+        color(): return the private variable color
+        is_valid_placement(): returns Ture or False for basic placement conditions
+    '''
     def __init__(self, color: PlayerColors):
         # uses setter to check errors
         if not isinstance(color, PlayerColors):
@@ -15,14 +30,34 @@ class Placeble(ABC):
 
     @property
     def color(self) -> PlayerColors:
+        '''
+        Returns the private variable color
+
+        Returns:
+            PlayerColors: instance of player color that represents the players color either black or white
+        '''
         return self.__color
 
     @abstractmethod
     def __str__(self) -> str:
+        '''
+        Starts the string method to be inherited by GamePiece
+
+        Returns:
+             str: The name of the GamePiece
+        '''
         pass
 
     @abstractmethod
     def is_valid_placement(self, pos: Position, board) -> bool:
+        '''
+        Returns Ture or False for basic placement conditions
+
+        Args:
+            pos(Position): the coordinates of the desired placement
+            board(list[list]): The current board state
+
+        '''
         # Check pos is a Position instance
         if not isinstance(pos, Position):
             return False
